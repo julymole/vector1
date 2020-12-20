c37fdfd7406a8ac6f4a54534fd4f2be080b710ec
julymole <64532834+julymole@users.noreply.github.com>
2020-12-20 17:18:31 +0300

    +main,footer

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,22 +1,53 @@
 //@@include('alert.js')
+@@include('slick.js');
 $(document).ready(function (event) {
-	
+
 	$('body').on('click', '.burger', function (event) {
 		event.preventDefault();
 		$('.burger, .header__wrap').toggleClass('active');
 		$('.header__menu-logo').slideToggle(2000);
 	})
-	
+
 	$('.header__row-tel-hidden').hide();
-	
+
 	$('body').on('mouseenter', '.header__row-tel-wrap', function (event) {
 		$('.header__row-tel-hidden').slideDown(1000);
 	})
 	$('body').on('mouseleave', '.header__row-tel-wrap', function (event) {
 		$('.header__row-tel-hidden').slideUp(1000);
 	})
-	
+
 	$('body').on('click', '.header__row-tel-arrow', function (event) {
 		$('.header__row-tel-hidden').slideToggle(1000);
 	})
+
+
+
+
+
+	$('.customers-slider').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		speed: 1000,
+		easing: 'ease',
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 1390,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},{
+				breakpoint: 850,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+		]
+	});
+
 });
